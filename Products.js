22f981ef617bd0563938c9ff1e1b5bc90700e0f7
Cristina <cristinabandeira82@gmail.com>
2025-02-05 01:35:47 +0000

    improvements 1.2

--- Products.js
+++ Products.js
@@ -96,8 +96,6 @@ const products = [
     1.29,
     1.79,
     28,
-    'Fruits',
-
     './assets/image/review-4S.png',
     5
   ),
@@ -141,17 +139,14 @@ const renderProducts = (products, cardIds, containerSelector, columnClass) => {
             <div class="card">
               <span class="badge card-badge bg-danger">${product.discount}%</span>
               <span class="position-absolute wishlist-icon"> <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg></span>
-              <div class="d-flex align-items-center ">
+              <div class="d-flex  ">
                 <img src="${product.image}" class="card-img" alt="${product.name}" />
               </div>
-              
               <div class=" card-body d-flex m-0 p-0">
-               
                 <p class=" card-title m-0">${product.name}</p>
                 <div class='d-flex card-review'>
-                  <img src="${product.rating}" class="d-flex me-3  " alt="${product.rating}" />
+                  <img src="${product.rating}" class="d-flex me-3 " alt="${product.rating}" />
                   <p >${product.reviewCount}</p>
-                  
                 </div>
                <div class="card-price">
                   <h6 class="card-price1 m-0">€${product.price}
