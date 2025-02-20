b80095931c6f4554e169aaf368d990d9996cc531
Cristina <cristinabandeira82@gmail.com>
2025-02-20 14:20:35 +0000

    refactor SVG icons and update banner text for improved clarity and consistency

--- Banner.js
+++ Banner.js
@@ -11,77 +11,77 @@ const banners = [
   new Banner(
     1,
     'Only this week',
-    'Quality eggs at an <br> affordable price',
+    'Quality eggs at an  affordable price',
     'Eat one every day',
     './assets/image/banner-01.png'
   ),
   new Banner(
     2,
     'Only this week',
-    'Snacks that nourishes <br> our mind and body',
+    'Snacks that nourishes  our mind and body',
     'Shine the mourning',
     './assets/image/banner-02.png'
   ),
   new Banner(
     3,
     'Only this week',
-    'Unbeatable quality,<br> unbeatable prices.',
+    'Unbeatable quality, unbeatable prices.',
     'Only this week. Dont miss...',
     './assets/image/banner-03.png'
   ),
   new Banner(
     4,
     'Only this week',
-    'Provide you experienced <br> quality products',
+    'Provide you experienced  quality products',
     'Feed your family the best',
     './assets/image/banner-04.png'
   ),
   new Banner(
     5,
     'Only this week',
-    'Shopping with us for better <br> quality and the best price ',
+    'Shopping with us for better  quality and the best price ',
     'Only this week. Don’t miss...',
     './assets/image/banner-05.png'
   ),
   new Banner(
     6,
     'Only this week',
-    'Get the best quality products <br> at the lowest prices',
+    'Get the best quality products at the lowest prices',
     'A different kind of grocery store',
     './assets/image/banner-06.png'
   ),
   new Banner(
     7,
     'Only this week',
-    'Where you get your all favorite <br> brands under one roof ',
+    'Where you get your all favorite  brands under one roof ',
     'Only this week. Dont miss...',
     './assets/image/banner-07.png'
   ),
   new Banner(
     8,
     'Only this week',
-    'Make your grocery <br> shopping easy with us ',
+    'Make your grocery shopping easy with us ',
     'Only this week. Don’t miss...',
     './assets/image/banner-08.png'
   ),
   new Banner(
     9,
     'Only this week',
-    'Get your everyday <br> needs here with us ',
+    'Get your everyday needs here with us ',
     'A different kind of grocery store',
     './assets/image/banner-09.png'
   ),
   new Banner(
     10,
     'Only this week',
-    'we are always here to help <br> you with your grocery',
+    'we are always here to help you with your grocery',
     'Eat one every day',
     './assets/image/banner-10.png'
   ),
   new Banner(
     11,
     'Only this week',
-    'With your favorite food,<br> we will make your mood',
+    'With your favorite food, we will make your mood',
     'Only this week. Don’t miss...',
     './assets/image/banner-11.png'
   ),
@@ -103,7 +103,7 @@ const renderBanner = (banners, bannerIds, containerSelector, columnClass) => {
                   <p>${banner.paragraph2}</p>
                   <button class=''>Shop now -></button>
                 </div>
-                <img class="" src="${banner.image}" alt="banner image" />
+                <img class="" src="${banner.image}" alt="${banner.text}" />
              
             `;
       container.appendChild(bannerCard);
--- Products.js
+++ Products.js
@@ -25,73 +25,67 @@ const products = [
   ),
   new Product(
     2,
-    'Fresh strawberry hand-picked –  per unit',
+    'Fresh Strawberries - Per Package',
     './assets/image/product5.png',
     8.99,
     9.99,
     11,
-
     './assets/image/review-4S.png',
     5
   ),
   new Product(
     3,
-    'Simply Orange Pulp Free Juice– 52 fl oz',
+    'Simply Orange Juice – 52 fl oz',
     './assets/image/product2.png',
     2.45,
     4.13,
     41,
-
     './assets/image/review-4S.png',
     5
   ),
   new Product(
     4,
-    'Cantaloupe Melon Fresh Organic Cut',
+    'Organic Cantaloupe Melon - Fresh Cut',
     './assets/image/product4.png',
     1.25,
     2.98,
     59,
-
     './assets/image/review-4S.png',
     5
   ),
   new Product(
     5,
-    'All Natural 85_15 Ground Beef –1lb',
+    'Beef Steak Mince – 1lb',
     './assets/image/product3.png',
     14.12,
     17.12,
     18,
-
     './assets/image/review-4S.png',
     5
   ),
   new Product(
     6,
-    'California Pizza Kitchen Margherita, Crispy Thin',
+    'California Margherita Pizza - Crispy Thin',
     './assets/image/product6.png',
     3.99,
     5.49,
     27,
-
     './assets/image/review-4S.png',
-    5
+    4.5
   ),
   new Product(
     7,
-    '100 Percent Apple Juice – 64fl oz Bottle',
+    '100% Apple Juice – 64fl oz Bottle',
     './assets/image/product7.png',
     4.99,
     6.99,
     29,
-
     './assets/image/review-4S.png',
-    5
+    3
   ),
   new Product(
     8,
-    'Marketside Fresh Organic Bananas, Bunch',
+    'Fresh Organic Bananas - Bunch',
     './assets/image/product8.png',
     1.29,
     1.79,
@@ -101,7 +95,7 @@ const products = [
   ),
   new Product(
     9,
-    'Vitaminwater zero sugar squeezed electrolyte…',
+    'Vitamin Water - Zero sugar ',
     './assets/image/product9.png',
     4.49,
     5.99,
@@ -135,13 +129,9 @@ const renderProducts = (products, cardIds, containerSelector, columnClass) => {
       productCard.classList.add('product-card');
 
       productCard.innerHTML = `
-         
-            <div class="d-flex card">
               <span class="badge card-badge bg-danger">${product.discount}%</span>
               <span class="position-absolute wishlist-icon"> <svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg></span>
-              <div class=" ">
                 <img src="${product.image}" class="card-img" alt="${product.name}" />
-              </div>
               <div class=" card-body d-flex m-0 p-0">
                 <p class=" card-title ">${product.name}</p>
                 <div class='d-flex card-review'>
@@ -156,18 +146,14 @@ const renderProducts = (products, cardIds, containerSelector, columnClass) => {
                 <button class="card-button">Add to the cart  <span class='plus ms-5'> + </span>  </button>
                 </div>
               </div>
-            </div>
-          
       `;
-
       container.appendChild(productCard);
     }
   });
 };
-
 // Use the renderProducts function
 document.addEventListener('DOMContentLoaded', () => {
-  renderProducts(products, [1, 2, 3, 4, 5, 6], '.productRow-01');
+  renderProducts(products, [1, 7, 3, 4, 5, 6], '.productRow-01');
   renderProducts(products, [1, 2, 3, 4, 5, 6], '.productRow-02');
   renderProducts(products, [1, 2], '.productRow-03');
   renderProducts(products, [3, 4], '.productRow-04');
